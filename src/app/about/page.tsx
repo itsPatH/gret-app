d490f05ef7bd22367079e2279043a4f291d063d6
Patricia Herrera <herrerapatriciadg@gmail.com>
2025-03-22 17:49:20 -0300

    fix(about): update profile image source and remove unused file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,13 +6,13 @@ const AboutMe = () => {
   return (
     <section className="flex flex-col items-center justify-center py-12 pt-20 md:flex-row md:gap-10">
       <div className="w-48 h-48 overflow-hidden border-4 rounded-full shadow-lg border-color4">
-        <Image
-          src="/images/profile.jpg"
-          alt="Dra. Gretzalid Meléndez"
-          width={192}
-          height={192}
-          className="object-cover w-full h-full"
-        />
+      <Image
+  src="/images/profilephoto.png"
+  alt="Dra. Gretzalid Meléndez"
+  width={192}
+  height={192}
+  className="object-cover w-full h-full"
+/>
       </div>
 
       <div className="max-w-lg mt-6 text-center md:text-left md:mt-0">
